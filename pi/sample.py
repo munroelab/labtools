0e719ed462cb2eb2a911ff0bc04eaeb91022022c
evankielley <evankielley@gmail.com>
2016-06-29 11:11:08 -0230

    modifying script to write data to a file instead of just printing to screen

--- pi/sample.py
+++ pi/sample.py
@@ -16,23 +16,26 @@ import datetime
 import u3
 
 def main():
-
+    f = open("cup.txt", "w")
     cup_number = raw_input("Enter cup number: ")
     cup_density = raw_input("Enter its density: ")
- 
+    print "Cup no.: ", cup_number, "\n", "Density: ", cup_density
+    f.write(cup_number, cup_density) 
+
     # Set up LabJack
     d = u3.U3()
     # print d.configU3()
     AIN0_REGISTER = 0
-    print "Cup no.: ", cup_number, "\n", "Density: ", cup_density
     
     while True:
         sample = d.getAIN(0)
         now = datetime.datetime.now()
         print now, sample
-
+        f.write(now, sample)
         time.sleep(0.1)
 
 
 if __name__ == "__main__":
     main()
+
+f.close()
